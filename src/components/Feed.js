83fb921ea09da6fe3a5d3bbce46f9799557cd6a8
Paul Pessoa <paulmspessoa@gmail.com>
2023-07-13 10:15:31 -0300

    config feed

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,12 +1,18 @@
 import Thread from "./Thread";
 
-const Feed = () => {
+const Feed = ({user, filteredThreads }) => {
     return (
       <div className="feed">
-        <Thread/>
+        {filteredThreads && filteredThreads.map(filteredThread => 
+        <Thread key={filteredThread.id} user={user} filteredThread={filteredThread}/>
+      )}       
       </div>
     );
   }
   
   export default Feed;
+  
+
+
+
   
