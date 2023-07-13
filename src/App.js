8d5f3b5b8a9f40f3b4423c05d1cd17df42c78d3a
Paul Pessoa <74559558+paulpessoa@users.noreply.github.com>
2023-07-13 10:51:31 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import { useState, useEffect } from "react";
 
 const App = () => {
 
-  const api = process.env.REACT_APP_API ?? process.env.REACT_APP_API_LOCAL
+  const api = https://react-threads-api.vercel.app/"
 
   const [user, setUser ] = useState(null)
   const [threads, setThreads ] = useState(null)
