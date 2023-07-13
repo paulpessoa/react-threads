4464599d4aa4ee11b5521e55f1079f4f5885fcc9
Paul Pessoa <paulmspessoa@gmail.com>
2023-07-13 10:15:05 -0300

    config main app screen

--- src/App.js
+++ src/App.js
@@ -1,16 +1,78 @@
 import Nav from "./components/Nav";
 import Header from "./components/Header";
 import Feed from "./components/Feed";
-import PopUp from "./components/PopUp";
+// import PopUp from "./components/PopUp";
+
+import { useState, useEffect } from "react";
 
 const App = () => {
+  const [user, setUser ] = useState(null)
+  const [threads, setThreads ] = useState(null)
+  const [viewThreadsFeed, setViewThreadsFeed ] = useState(true)
+  const [filteredThreads, setFilteredThreads] = useState(null)
+
+  const userId = "aa2902ac-b250-48bd-af47-0b8dcc141b8d"
+
+  const getUser = async () => {
+    try {
+      const response = await fetch (`http://localhost:3000/users?user_uuid=${userId}`)
+      const data = await response.json()
+      setUser(data[0])
+    } catch(error) {
+      console.error(error)
+    }
+  }
+
+  const getThreads = async () => {
+    try {
+      const response = await fetch(`http://localhost:3000/threads?trhead_from=${userId}`)
+      const data = await response.json()
+      setThreads(data)
+  } catch(error) {
+      console.error(error)
+    }
+  }
+
+  const getThreadsFeed = async (viewThreadsFeed, threads, setFilteredThreads) => {
+    if (viewThreadsFeed) {
+      const standAloneThreads = threads?.filter(thread => thread.reply_to === null)
+      setFilteredThreads(standAloneThreads);
+    }
+    if (!viewThreadsFeed) {
+      const replyThreads = threads?.filter(thread => thread.reply_to !== null)
+      setFilteredThreads(replyThreads);
+    }
+  };
+
+  useEffect(() => {
+    getUser()
+    getThreads()
+  }, [])
+
+  useEffect(() => {
+    getThreadsFeed(viewThreadsFeed, threads, setFilteredThreads);
+  }, [user, threads, viewThreadsFeed]);
+
+
+  console.log(filteredThreads)
+
   return (
+    <>
+    {user && 
     <div className="app">
-      <Nav />
-      <Header />
-      <Feed />
-      <PopUp />
-    </div>
+      <Nav url={user.instagram_url}/>
+      <Header
+        user={user}
+        viewThreadsFeed={viewThreadsFeed}
+        setViewThreadsFeed={setViewThreadsFeed}/>
+      <Feed
+        user={user}
+        filteredThreads={filteredThreads}
+      />
+      {/* <PopUp /> */}
+    </div> 
+    }
+    </>
   );
 };
 
